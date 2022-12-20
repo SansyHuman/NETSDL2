a395fd36e5887c0d28507eca606d790e484965d4
SansyHuman <suboo0308@gmail.com>
2022-12-20 13:24:34 +0900

    Moved PixelFormatWrapper and PaletteWrapper to Ext.

---
+++ NETSDL2_Ext/Video/PaletteWrapper.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NETSDL2.Video
+{
+
+    /// <summary>
+    /// Safe wrapper of <see cref="NETSDL2.Video.Palette"/> pointer.
+    /// </summary>
+    public unsafe class PaletteWrapper : IDisposable
+    {
+        private Palette* palette;
+        private bool disposedValue;
+
+        public PaletteWrapper(Palette* palette)
+        {
+            this.palette = palette;
+        }
+
+        /// <summary>
+        /// Get the native pallete.
+        /// </summary>
+        public Palette* Palette
+        {
+            get
+            {
+                return palette;
+            }
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+
+                }
+
+                Pixels.FreePalette(palette);
+                palette = null;
+                disposedValue = true;
+            }
+        }
+
+        ~PaletteWrapper()
+        {
+            // 이 코드를 변경하지 마세요. 'Dispose(bool disposing)' 메서드에 정리 코드를 입력합니다.
+            Dispose(disposing: false);
+        }
+
+        public void Dispose()
+        {
+            // 이 코드를 변경하지 마세요. 'Dispose(bool disposing)' 메서드에 정리 코드를 입력합니다.
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        public static implicit operator Palette*(PaletteWrapper wrapper)
+        {
+            return wrapper.palette;
+        }
+    }
+}
---
+++ NETSDL2_Ext/Video/PixelFormatWrapper.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NETSDL2.Video
+{
+
+	/// <summary>
+	/// Safe wrapper of <see cref="NETSDL2.Video.PixelFormat"/> pointer.
+	/// </summary>
+	public unsafe class PixelFormatWrapper : IDisposable
+    {
+		private PixelFormat* format;
+        private bool disposedValue;
+
+        public PixelFormatWrapper(PixelFormat* format)
+        {
+			this.format = format;
+        }
+
+        /// <summary>
+        /// Get the native pixel format.
+        /// </summary>
+        public PixelFormat* PixelFormat
+        {
+            get
+            {
+                return format;
+            }
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    
+                }
+
+                Pixels.FreeFormat(format);
+                format = null;
+                disposedValue = true;
+            }
+        }
+
+        ~PixelFormatWrapper()
+        {
+            // 이 코드를 변경하지 마세요. 'Dispose(bool disposing)' 메서드에 정리 코드를 입력합니다.
+            Dispose(disposing: false);
+        }
+
+        public void Dispose()
+        {
+            // 이 코드를 변경하지 마세요. 'Dispose(bool disposing)' 메서드에 정리 코드를 입력합니다.
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        public static implicit operator PixelFormat*(PixelFormatWrapper wrapper)
+        {
+            return wrapper.format;
+        }
+    }
+}
